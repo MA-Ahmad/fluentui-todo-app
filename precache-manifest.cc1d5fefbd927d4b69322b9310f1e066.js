600d8cbeafe9eda591e34d12ba3233738392358a
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-08-31 08:45:34 -0700

    Updates

---
+++ precache-manifest.cc1d5fefbd927d4b69322b9310f1e066.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f453d2236fcda6e3af08e3739ddcc78f",
+    "url": "/fluentui-todo-app/index.html"
+  },
+  {
+    "revision": "38e6581514affde3cd71",
+    "url": "/fluentui-todo-app/static/js/2.a4e6f6d4.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/fluentui-todo-app/static/js/2.a4e6f6d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "388174ddfa24f47dfe2f",
+    "url": "/fluentui-todo-app/static/js/main.46389c30.chunk.js"
+  },
+  {
+    "revision": "83ae1558a9b3c89e4e73",
+    "url": "/fluentui-todo-app/static/js/runtime-main.830cc4b5.js"
+  }
+]);
